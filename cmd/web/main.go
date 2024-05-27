f5b6bb954a5e9dfafdb7ba9153b4a56b51943250
bachtg03 <giabach9102@gmail.com>
2024-05-27 16:49:10 +0700

    Truyen full crawl categories

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,59 +2,16 @@ package main
 
 import (
 	"fmt"
-	"github.com/gocolly/colly/v2"
-	"log"
-	"strings"
-	"time"
+	"novel_crawler/internal"
 )
 
 func main() {
-	c := colly.NewCollector(
-		colly.AllowedDomains("truyenfull.vn"),
-		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"),
-		colly.Async(true),
-		colly.MaxDepth(1),
-	)
 
-	// Set up event handlers
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting: ", r.URL)
-	})
+	truyenFull := &internal.TruyenFull{}
+	listCategories := truyenFull.GetCategories()
 
-	c.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong: ", err)
-	})
-
-	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("Page visited: ", r.Request.URL)
-	})
-
-	c.OnHTML("a", func(e *colly.HTMLElement) {
-		// Printing all URLs associated with the a links in the page
-		str := e.Attr("href")
-		if strings.Contains(str, "the-loai") {
-			fmt.Println(e.Attr("title"))
-		}
-
-	})
-
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println(r.Request.URL, "scraped!")
-	})
-
-	// Adding a delay between requests
-	c.Limit(&colly.LimitRule{
-		DomainGlob:  "*",
-		Delay:       2 * time.Second,
-		RandomDelay: 1 * time.Second,
-	})
-
-	// Now visit the page
-	err := c.Visit("https://truyenfull.vn/")
-	if err != nil {
-		fmt.Println("err->", err)
+	for _, v := range listCategories {
+		fmt.Println(v)
 	}
 
-	// Wait until all asynchronous tasks are finished
-	c.Wait()
 }
