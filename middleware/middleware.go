375a7b72c43622563309315a107da7a1b230a121
21120450 <thehien08112003@gmail.com>
2024-06-15 12:55:01 +0700

    update cors

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -7,7 +7,9 @@ import (
 func CorsMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
+		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization")
+		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		ctx.Next()
 	}
 }
